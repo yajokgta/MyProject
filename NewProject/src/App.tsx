cea28c3ba74b21f56048fe89d3c0af86aa0e4002
Kitisak <Kitisak@techconsbiz.com>
2023-11-14 19:34:33 +0700

    .

--- NewProject/src/App.tsx
+++ NewProject/src/App.tsx
@@ -7,21 +7,35 @@ import { Button } from 'antd';
 import React from 'react';
 
 function App() {
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+  function login() {
+    console.log(Username, Password)
+  }
+
+  const handleUsername = (x: any) => {
+    setUsername(x.target.value);
+  };
+  const handlePassword = (x: any) => {
+    setPassword(x.target.value);
+  };
   return (
-    <div>
+    <div style={{ border: "3px solid #E7E7E7", borderRadius: "3px" }}>
       <div className="card">
-        <Input style={{width:"200px"}} placeholder='Username'></Input>
+        <Input style={{ width: "200px" }} placeholder='Username' value={Username} onChange={handleUsername}></Input>
       </div>
-      <div>
-      <Input style={{width:"200px"}} placeholder='Password'></Input>
+      <div className="card">
+        <Input type='Password' style={{ width: "200px" }} placeholder='Password' value={Password} onChange={handlePassword}></Input>
       </div>
       <div className='card'>
-       <Button>
-        Login
-       </Button>
-       <Button>
-        Register
-       </Button>
+        <div>
+          <Button className='input' onClick={() => login()}>
+            Login
+          </Button>
+          <Button className='input'>
+            Register
+          </Button>
+        </div>
       </div>
     </div>
   )
