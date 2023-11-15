6ec41482deefb4c335f56bfc18f0e8e1607a4670
Kitisak <Kitisak@techconsbiz.com>
2023-11-16 00:00:44 +0700

    .

---
+++ NewProject/src/Screen/DefaltScreen.tsx
@@ -0,0 +1,24 @@
+import { Menu, Breadcrumb } from "antd";
+import Layout, { Header } from "antd/es/layout/layout";
+
+export default function DefaultScreen() {
+    return (
+        <Layout className="layout">
+            <Header style={{ display: "flex", alignItems: "center" }}>
+                <div className="demo-logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={["2"]}
+                    items={new Array(3).fill(null).map((_, index) => {
+                        const key = index + 1;
+                        return {
+                            key,
+                            label: `ควย ${key}`
+                        };
+                    })}
+                />
+            </Header>
+        </Layout>
+    )
+}
--- NewProject/src/main.tsx
+++ NewProject/src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import LoginScreen from './Screen/LoginScreen.tsx'
+import DefaltScreen from './Screen/DefaltScreen.tsx'
 import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={CheckLogin()} />
+        <Route path='/Login' element={<LoginScreen />} />
+        <Route path="/Defalt" element={<DefaltScreen />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
+
+export function CheckLogin() {
+  if (!localStorage.getItem("token")) {
+    return (
+      <LoginScreen />
+    );
+  }
+  else {
+    return (
+      <DefaltScreen />
+    )
+  }
+}
