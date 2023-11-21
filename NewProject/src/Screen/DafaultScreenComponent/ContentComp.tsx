1aa0f0edfd8db3e4644b54bc9a05879466bea37c
Kitisak <kitisak@techconsbiz.com>
2023-11-21 17:48:06 +0700

    .

--- NewProject/src/Screen/DafaultScreenComponent/ContentComp.tsx
+++ NewProject/src/Screen/DafaultScreenComponent/ContentComp.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Col, Row } from "antd";
 import { Content } from "antd/es/layout/layout";
 
 interface Oder {
@@ -11,37 +11,49 @@ interface Oder {
 export default function ContentComp() {
     function oderBlock(oder: Oder) {
         return (
-            <Card style={{width:"512px", height:"512px"}} cover={<img src={oder.img} />}>
-                <div className="oderdescription">
-                    <p>Test</p>
-                </div>
-            </Card>
+            <Col span={6}>
+                <Card style={{ height: "512px" }} cover={<img src={oder.img} />}>
+                    <div className="oderdescription">
+                        <p>Test</p>
+                    </div>
+                </Card>
+            </Col>
         );
     }
 
-    let listOder:Array<Oder> = new Array;
+    let listOder: Array<Oder> = new Array;
 
     listOder.push({
         img: "test",
         name: "",
         price: 10,
         stock: 0
-    },{
+    }, {
         img: "xxx",
         name: "",
         price: 10,
         stock: 0
-    },{
+    }, {
         img: "xxx",
         name: "",
         price: 10,
         stock: 0
-    },{
+    }, {
         img: "xxx",
         name: "",
         price: 10,
         stock: 0
-    },{
+    }, {
+        img: "xxx",
+        name: "",
+        price: 10,
+        stock: 0
+    }, {
+        img: "xxx",
+        name: "",
+        price: 10,
+        stock: 0
+    }, {
         img: "xxx",
         name: "",
         price: 10,
@@ -49,10 +61,12 @@ export default function ContentComp() {
     });
 
     return (
-        <Content style={{ position: "relative", display:"grid" }}>
-            {listOder.map((x)=>{
-                return(oderBlock(x))
-            })}
+        <Content>
+            <Row style={{ justifyContent: "flex-start" }}>
+                {listOder.map((x) => {
+                    return (oderBlock(x))
+                })}
+            </Row>
         </Content>
     );
 }
